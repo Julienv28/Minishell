c667077271f960dae0665db7f5ab13856810101f
opique42 <oceanepique7@gmail.com>
2025-05-07 08:07:33 +0200

    Revert "pipes changed, different logic"

--- srcs/pipes/pipex_simple.c
+++ srcs/pipes/pipex_simple.c
@@ -6,92 +6,92 @@
 /*   By: juvitry <juvitry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 12:02:35 by juvitry           #+#    #+#             */
-/*   Updated: 2025/05/06 13:24:07 by juvitry          ###   ########.fr       */
+/*   Updated: 2025/05/02 15:12:39 by juvitry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-// static void	child_proc(t_com_list *command, char **args, char **envcp, int *fd)
-// {
-// 	dup2(fd[1], STDOUT_FILENO);
-// 	close(fd[0]);
-// 	close(fd[1]);
-// 	if (args && is_builting(args[0]) == 0)
-// 		exec_builting(args, &envcp);
-// 	else
-// 		exec_cmd(command, envcp);
-// }
+static void	child_proc(t_com_list *command, char **args, char **envcp, int *fd)
+{
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+	if (args && is_builting(args[0]) == 0)
+		exec_builting(args, &envcp);
+	else
+		exec_cmd(command, envcp);
+}
 
-// static void	parent_proc(t_com_list *command, char **args, char **envcp, int *fd)
-// {
-// 	dup2(fd[0], STDIN_FILENO);
-// 	close(fd[1]);
-// 	close(fd[0]);
-// 	if (args && is_builting(args[0]) == 0 && ft_strcmp(args[0], "cd") != 0)
-// 		exec_builting(args, &envcp);
-// 	else if (ft_strcmp(args[0], "cd") == 0)
-// 		;
-// 	else
-// 		exec_cmd(command, envcp);
-// }
+static void	parent_proc(t_com_list *command, char **args, char **envcp, int *fd)
+{
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[1]);
+	close(fd[0]);
+	if (args && is_builting(args[0]) == 0 && ft_strcmp(args[0], "cd") != 0)
+		exec_builting(args, &envcp);
+	else if (ft_strcmp(args[0], "cd") == 0)
+		;
+	else
+		exec_cmd(command, envcp);
+}
 
-// void	split_args_for_pipe(char **args, char ***left, char ***right)
-// {
-// 	int		i;
-// 	int		left_len = 0;
-// 	int		right_len = 0;
+void	split_args_for_pipe(char **args, char ***left, char ***right)
+{
+	int		i;
+	int		left_len = 0;
+	int		right_len = 0;
 
-// 	while (args[left_len] && ft_strcmp(args[left_len], "|") != 0)
-// 		left_len++;
-// 	if (!args[left_len])
-// 		return ;
-// 	i = left_len + 1;
-// 	while (args[i++])
-// 		right_len++;
-// 	*left = malloc(sizeof(char *) * (left_len + 1));
-// 	*right = malloc(sizeof(char *) * (right_len + 1));
-// 	i = 0;
-// 	while (i < left_len)
-// 	{
-// 		(*left)[i] = strdup(args[i]);
-// 		i++;
-// 	}
-// 	(*left)[i] = NULL;
-// 	i = 0;
-// 	while (args[left_len + 1 + i])
-// 	{
-// 		(*right)[i] = strdup(args[left_len + 1 + i]);
-// 		i++;
-// 	}
-// 	(*right)[i] = NULL;
-// }
+	while (args[left_len] && ft_strcmp(args[left_len], "|") != 0)
+		left_len++;
+	if (!args[left_len])
+		return ;
+	i = left_len + 1;
+	while (args[i++])
+		right_len++;
+	*left = malloc(sizeof(char *) * (left_len + 1));
+	*right = malloc(sizeof(char *) * (right_len + 1));
+	i = 0;
+	while (i < left_len)
+	{
+		(*left)[i] = strdup(args[i]);
+		i++;
+	}
+	(*left)[i] = NULL;
+	i = 0;
+	while (args[left_len + 1 + i])
+	{
+		(*right)[i] = strdup(args[left_len + 1 + i]);
+		i++;
+	}
+	(*right)[i] = NULL;
+}
 
-// void	pipex_simple(t_com_list *command, char **args, char **envcp)
-// {
-// 	int		fd[2];
-// 	pid_t	pid1;
-// 	pid_t	pid2;
-// 	char	**left_args;
-// 	char	**right_args;
+void	pipex_simple(t_com_list *command, char **args, char **envcp)
+{
+	int		fd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+	char	**left_args;
+	char	**right_args;
 
-// 	split_args_for_pipe(args, &left_args, &right_args);
-// 	if (pipe(fd) == -1)
-// 		exit_error();
-// 	pid1 = fork();
-// 	if (pid1 == -1)
-// 		exit_error();
-// 	if (pid1 == 0)
-// 		child_proc(command, left_args, envcp, fd);
-// 	pid2 = fork();
-// 	if (pid2 == -1)
-// 		exit_error();
-// 	if (pid2 == 0)
-// 	{
-// 		parent_proc(command, right_args, envcp, fd);
-// 	}
-// 	close(fd[0]);
-// 	close(fd[1]);
-// 	waitpid(pid1, NULL, 0);
-// 	waitpid(pid2, NULL, 0);
-// }
+	split_args_for_pipe(args, &left_args, &right_args);
+	if (pipe(fd) == -1)
+		exit_error();
+	pid1 = fork();
+	if (pid1 == -1)
+		exit_error();
+	if (pid1 == 0)
+		child_proc(command, left_args, envcp, fd);
+	pid2 = fork();
+	if (pid2 == -1)
+		exit_error();
+	if (pid2 == 0)
+	{
+		parent_proc(command, right_args, envcp, fd);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+}
--- srcs/pipes_manager.c
+++ srcs/pipes_manager.c
@@ -6,7 +6,7 @@
 /*   By: juvitry <juvitry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 11:33:17 by juvitry           #+#    #+#             */
-/*   Updated: 2025/05/06 13:08:46 by juvitry          ###   ########.fr       */
+/*   Updated: 2025/05/06 11:46:35 by juvitry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,85 +71,3 @@ void parse_pipes(t_com_list *list, t_minishell *mini)
 		complex_pipex();
 }*/
 
-static void	remove_args(char **args, int index, int count)
-{
-	int	i;
-	int	j;
-
-	i = index;
-	j = 0;
-	while (j < count && args[i + j])
-	{
-		free(args[i + j]);
-		args[i + j] = NULL;
-		j++;
-	}
-	while (args[i + count])
-	{
-		args[i] = args[i + count];
-		i++;
-	}
-	args[i] = NULL;
-	while (args[i])
-	{
-		free(args[i]);
-		args[i++] = NULL;
-	}
-}
-
-static void	parse_redirections2(t_com_list *node)
-{
-	int		i;
-	char	**args;
-
-	i = 0;
-	args = node->args;
-	while (args && args[i])
-	{
-		if (ft_strcmp(args[i], "<") == 0 && args[i + 1])
-		{
-			node->infile = ft_strdup (args[i + 1]);
-			remove_args(args, i, 2);
-		}
-		else if (ft_strcmp(args[i], ">") == 0 && args[i + 1])
-		{
-			node->outfile = ft_strdup(args[i + 1]);
-			node->append = 0;
-			remove_args(args, i, 2);
-		}
-		else if (ft_strcmp(args[i], ">>") == 0 && args[i + 1])
-		{
-			node->outfile = ft_strdup(args[i + 1]);
-			node->append = 1;
-			remove_args(args, i, 2);
-		}
-	}
-}
-
-t_com_list	*parse_pipes(char *line)
-{
-	t_com_list	*head = NULL, *current = NULL;
-	char		**segments;
-	int			i = 0;
-
-	segments = split_pipe_respect_quotes(line);
-	while (segments[i])
-	{
-		t_com_list	*node = malloc(sizeof(t_com_list));
-		node->command = ft_strtrim(segments[i], " \t\n");
-		node->args = split_args(node->command, ' ');
-		node->infile = NULL;
-		node->outfile = NULL;
-		node->append = 0;
-		node->next = NULL;
-		parse_redirections2(node);
-		if (!head)
-			head = node;
-		else
-			current->next = node;
-		current = node;
-		i++;
-	}
-	free_tab(segments);
-	return (head);
-}
