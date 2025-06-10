859934f6a8b833c4efd7dc3aae3458e521cf3c6d
Julien Vitry <juvitry@f2r10s9.clusters.42paris.fr>
2025-06-10 11:26:18 +0200

    echo a la norme

--- srcs/commandes/echo.c
+++ srcs/commandes/echo.c
@@ -6,49 +6,11 @@
 /*   By: juvitry <juvitry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 16:42:01 by juvitry           #+#    #+#             */
-/*   Updated: 2025/06/10 11:09:29 by juvitry          ###   ########.fr       */
+/*   Updated: 2025/06/10 11:25:25 by juvitry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-static char	*unescape_backslashes(const char *str)
-{
-	int		i;
-	int		j;
-	int		count;
-	char	*res;
-
-	i = 0;
-	j = 0;
-	res = malloc(ft_strlen(str) + 1);
-	if (!res)
-		return (NULL);
-	while (str[i])
-	{
-		if (str[i] == '\\')
-		{
-			count = 0;
-			while (str[i + count] == '\\')
-				count++;
-			int pairs = count / 2;
-			while (pairs--)
-				res[j++] = '\\';
-			if (count % 2 == 1)
-			{
-				i += count;
-				if (str[i])
-					res[j++] = str[i++];
-			}
-			else
-				i += count;
-		}
-		else
-			res[j++] = str[i++];
-	}
-	res[j] = '\0';
-	return (res);
-}
 
 char	*build_spaced_result(char *arg, int i, int len, char **envcp)
 {
@@ -107,12 +69,27 @@ char	*add_space_if_needed(char *arg, char **envcp)
 	return (ft_strdup(arg));
 }
 
+static void	print_echo_arg(char *arg, char ***envcp)
+{
+	char	*unescaped;
+	char	*patched;
+
+	unescaped = unescape_backslashes(arg);
+	if (!unescaped)
+		return ;
+	patched = add_space_if_needed(unescaped, *envcp);
+	free(unescaped);
+	if (patched)
+	{
+		ft_putstr_fd(patched, STDOUT_FILENO);
+		free(patched);
+	}
+}
+
 void	ft_echo(char **args, char ***envcp)
 {
 	int		i;
 	int		newline;
-	char	*patched;
-	char	*unescaped;
 
 	i = 1;
 	newline = 1;
@@ -123,16 +100,7 @@ void	ft_echo(char **args, char ***envcp)
 	}
 	while (args[i])
 	{
-		unescaped = unescape_backslashes(args[i]);
-		if (!unescaped)
-			return ;
-		patched = add_space_if_needed(unescaped, *envcp);
-		free(unescaped);
-		if (patched)
-		{
-			ft_putstr_fd(patched, STDOUT_FILENO);
-			free (patched);
-		}
+		print_echo_arg(args[i], envcp);
 		if (args[i + 1])
 			ft_putchar_fd(' ', STDOUT_FILENO);
 		i++;
